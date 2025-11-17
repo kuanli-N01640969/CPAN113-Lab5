774c5ef2d92364324be25ef53ebc27eaf7f99859
Kuan Yi Li <kuan_li@outlook.com>
2025-11-17 17:08:12 -0500

    Part4, Module Import and Usage

---
+++ app.js
@@ -0,0 +1,5 @@
+import { add, subtract } from './mathModule.js';
+import { toUpperCase, toLowerCase } from './stringModule.js';
+ 
+console.log('Subtract: ', subtract(10, 3));  
+console.log('Lowercase: ', toLowerCase('HELLOWoRld'));  
