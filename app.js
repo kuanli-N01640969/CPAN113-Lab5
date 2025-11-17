6342706f0a2a71170ef64ec46c6c89dd7aa9d5be
Kuan Yi Li <kuan_li@outlook.com>
2025-11-17 17:15:21 -0500

    Part5, Adding Default Export

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import { add, subtract } from './mathModule.js';
+import multiply,{ add, subtract } from './mathModule.js';
 import { toUpperCase, toLowerCase } from './stringModule.js';
- 
+
+console.log('Multiply: ', multiply(2, 5)); 
 console.log('Subtract: ', subtract(10, 3));  
 console.log('Lowercase: ', toLowerCase('HELLOWoRld'));  
