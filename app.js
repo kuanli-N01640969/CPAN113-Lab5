00e9cc4b99ae3b9b8d0d1eafed7d60f71ac08bc9
Kuan Yi Li <kuan_li@outlook.com>
2025-11-17 17:51:35 -0500

    Part7, Combine Functions Across Modules

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-import multiply,{ add, subtract } from './mathModule.js';
+import multiply,{ add, subtract, addAndLogUpper } from './mathModule.js';
 import { toUpperCase, toLowerCase } from './stringModule.js';
 import { findMax, reverseArray } from './arrayModule.js';
+
 
 console.log('Multiply: ', multiply(2, 5)); 
 console.log('Subtract: ', subtract(10, 3));  
 console.log('Lowercase: ', toLowerCase('HELLOWoRld'));  
 console.log('Max: ', findMax([0, 2, 14, 26, 73]));  
 console.log('Reversed: ', reverseArray([2, 4, 6, 8]));  
+addAndLogUpper(10, 16);
--- mathModule.js
+++ mathModule.js
@@ -9,3 +9,11 @@ export function subtract(a, b){
 export default function multiply(a, b){
     return a * b;
 }
+
+import { toUpperCase } from './stringModule.js';
+
+export function addAndLogUpper(a, b) {
+    const result = add(a, b);
+    console.log(toUpperCase(result.toString()));
+}
+
